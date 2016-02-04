c9e2793806ebbe6629c8045ce4ae128496fdc5ef
Oopscurity <oopscurity@mail.ru>
2016-02-04 17:51:51 +0400

    Added the solution of 27th task (1 var; 2016)

---
+++ evich_2016/1/task_27.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main() {
+	const int DEVISOR = 3,
+			  MIN_TIME_INTERVAL = 5,
+              ERROR_CODE = 0;
+	int N,
+        lastIndex,
+        min = 1001,
+        minDivToSelf = 1001,
+        minDivToSelfIndex;
+
+	std::cin >> N;
+
+    int current,
+        currentIndex;
+	for (int i = 0; i < N; ++i) {
+        std::cin >> current;
+        currentIndex = i;
+
+        if ((current % DEVISOR == 0) && (current < minDivToSelf)) {
+            minDivToSelf = current;
+            minDivToSelfIndex = i;
+        }
+        if ((i - minDivToSelfIndex >= 5) && (current < min)) {
+            min = current;
+        }
+	}
+
+    std::cout << ((minDivToSelf < 1001) ? (min * minDivToSelf) : ERROR_CODE);
+    return 0;
+}
