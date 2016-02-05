45e9d6e78d2640bc4a7c26047e92a3e979f6104b
Oopscurity <oopscurity@mail.ru>
2016-02-05 15:50:19 +0400

    Solution - 27th task, 5 var (2016)

---
+++ evich_2016/5/task_27.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+struct Name {
+    std::string surname;
+    std::string initials;
+};
+
+int main() {
+    const int MAX_RATE = 100,
+              EXAMS_NUM = 3;
+
+    int N; // by condition: the number of students
+    std::cin >> N;
+    if (!N) {
+        return -1;
+    }
+
+    int minAverage = MAX_RATE * EXAMS_NUM + 1,
+        currentRate,
+        currentAverage = 0;
+
+    Name current;
+    std::vector<Name> minimals;
+    for (int i = 0; i < N; ++i, currentAverage = 0) {
+        std::getline(std::cin, current.surname, ' ');
+        std::getline(std::cin, current.initials, ' ');
+
+        for (int j = 0; j < EXAMS_NUM; ++j) {
+            std::cin >> currentRate;
+            currentAverage += currentRate;
+        }
+
+        if (currentAverage > minAverage)
+            continue;
+
+        if (currentAverage < minAverage) {
+            minimals.clear();
+            minimals.push_back(current);
+            minAverage = currentAverage;
+        } else {
+            minimals.push_back(current);
+        }
+    }
+
+    for (auto it = minimals.begin(); it != minimals.end(); ++it) {
+        std::cout << it->surname << ' ' << it->initials << ' ' << minAverage / 3; // probably '\n' is required
+    }
+    return 0;
+}
