8f61bfa305abc63b165d09c9aa2a485a0b4e45ae
Oopscurity <oopscurity@mail.ru>
2016-02-09 16:58:42 +0400

    Editing: 27 task, 14 var (2016) - format

--- evich_2016/14/task_27.cpp
+++ evich_2016/14/task_27.cpp
@@ -4,34 +4,34 @@
 #include <string>
 
 int main() {
-	std::string message;
+    std::string message;
 
-	char c;
-	while((c = std::cin.get()) != '\n') {
-		if ( isalpha(int(c)) ) {
-			message += char(toupper(c));
-		}
-	}
-	
-	int i = 0, // digit from char
-		counter = 0, // the position to insert new character
-	    currentPosition = 0; // origin of counting
-	char elem[2] = { '1', '\0' }; // for atoi
-	std::string result(message.size(), '\0');
-	
-	while((c = std::cin.get()) != '\n') {
-		if ( isdigit(c) ) {
-			elem[0] = c;
-			i = atoi(elem) - 1;
+    char c;
+    while((c = std::cin.get()) != '\n') {
+        if ( isalpha(int(c)) ) {
+            message += char(toupper(c));
+        }
+    }
+    
+    int i = 0, // digit from char
+        counter = 0, // the position to insert new character
+        currentPosition = 0; // origin of counting
+    char elem[2] = { '1', '\0' }; // for atoi
+    std::string result(message.size(), '\0');
+    
+    while((c = std::cin.get()) != '\n') {
+        if ( isdigit(c) ) {
+            elem[0] = c;
+            i = atoi(elem) - 1;
 
-			result[currentPosition + counter] = message[currentPosition + i];
-			++counter;
-		} else if ( c == ';' ) {
-			currentPosition = counter;
-			counter = 0;
-		}
-	}
+            result[currentPosition + counter] = message[currentPosition + i];
+            ++counter;
+        } else if ( c == ';' ) {
+            currentPosition = counter;
+            counter = 0;
+        }
+    }
 
-	std::cout << result;
-	return 0;
+    std::cout << result;
+    return 0;
 }
