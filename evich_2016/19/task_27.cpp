a5ad2a46d47f048b71c46d984f9146f92b1b6c1e
Oopscurity <oopscurity@mail.ru>
2016-02-11 17:00:00 +0400

    Solution: 27 task, 19 var (2016)

---
+++ evich_2016/19/task_27.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main() {
+    const double SELECTION_RATE = 0.05;
+    const int MAX_POSSIBLE_RATE = 20,
+              STONE_NAME_LIMIT = 20,
+              STONE_CODE_LIMIT = 15;
+    int N; // by condition: the number of stones
+    int stonesPerRate[MAX_POSSIBLE_RATE] = { 0 };
+    std::cin >> N;
+
+    int currentRate;
+    for (int i = 0; i < N; ++i) {
+        std::cin.ignore(STONE_NAME_LIMIT, ' ');
+        std::cin.ignore(STONE_CODE_LIMIT, ' ');
+        std::cin >> currentRate;
+        ++stonesPerRate[currentRate - 1];
+    }
+
+    int minRate = MAX_POSSIBLE_RATE,
+        allSuitableStones = (N * SELECTION_RATE > 1) ? N * SELECTION_RATE : 1,
+        already = 0;
+    do {
+        while ((already < allSuitableStones) && (stonesPerRate[minRate - 1] > 0)) {
+            --stonesPerRate[minRate - 1];
+            ++already;
+        }
+    } while ((already < allSuitableStones) && --minRate);
+
+    std::cout << minRate;
+    return 0;
+}
